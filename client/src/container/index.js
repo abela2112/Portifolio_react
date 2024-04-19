c91d74fc561bda69b72f35b21c66d305d21cce92
Abel Ayalew Abebe <103965027+abela2112@users.noreply.github.com>
2024-04-19 03:46:41 +0300

    fix issues related to about page

--- client/src/container/index.js
+++ client/src/container/index.js
@@ -4,4 +4,4 @@ export {default as Testimonial} from './testimonials/Testimonials'
 export {default as Skill} from './Skills/skills'
 export {default as Footer} from './Footer/Footer'
 export {default as Header} from './header/header'
-export { default as About } from "/About/About";
+export { default as About } from "./About/About";
